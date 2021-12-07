b49bbaf1de2d64b6159218c0d950569c4935a346
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-12-07 06:58:15 +0100

    signup works

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import './App.scss';
 function App() {
 	const [loginFormField_login, setLoginFormField_login] = useState('');
 	const [loginFormField_password, setLoginFormField_password] = useState('');
+
+	const [signupFormField_login, setSignupFormField_login] = useState('');
+	const [signupFormField_password1, setSignupFormField_password1] = useState('');
+	const [signupFormField_password2, setSignupFormField_password2] = useState('');
+	const [signupFormField_firstName, setSignupFormField_firstName] = useState('');
+	const [signupFormField_lastName, setSignupFormField_lastName] = useState('');
+	const [signupFormField_email, setSignupFormField_email] = useState('');
+
 	const [currentUser, setCurrentUser] = useState({});
 
 	useEffect(() => {
@@ -25,16 +33,15 @@ function App() {
 		return accessGroupArray.includes(accessGroup);
 	}
 
+	// LOGIN FORM FIELD HANDLERS
 	const handle_loginFormField_login = (e) => {
 		let login = e.target.value;
 		setLoginFormField_login(login);
 	}
-
 	const handle_loginFormField_password = (e) => {
 		let password = e.target.value;
 		setLoginFormField_login(password);
 	}
-
 	const handle_loginForm_loginButton = async (e) => {
 		e.preventDefault();
 		const requestOptions = {
@@ -51,6 +58,62 @@ function App() {
 			setLoginFormField_password('');
 		}
 	}
+
+	// SIGNUP FORM FIELD HANDLERS
+	const handle_signupFormField_login = (e) => {
+		let login = e.target.value;
+		setSignupFormField_login(login);
+	}
+	const handle_signupFormField_password1 = (e) => {
+		let password1 = e.target.value;
+		setSignupFormField_password1(password1);
+	}
+	const handle_signupFormField_password2 = (e) => {
+		let password2 = e.target.value;
+		setSignupFormField_password2(password2);
+	}
+	const handle_signupFormField_firstName = (e) => {
+		let firstName = e.target.value;
+		setSignupFormField_firstName(firstName);
+	}
+	const handle_signupFormField_lastName = (e) => {
+		let lastName = e.target.value;
+		setSignupFormField_lastName(lastName);
+	}
+	const handle_signupFormField_email = (e) => {
+		let email = e.target.value;
+		setSignupFormField_email(email);
+	}
+	const handle_signupForm_signupButton = async (e) => {
+		e.preventDefault();
+		const requestOptions = {
+			method: 'POST',
+			credentials: "include",
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(
+				{
+					login: signupFormField_login,
+					password1: signupFormField_password1,
+					password2: signupFormField_password2,
+					firstName: signupFormField_firstName,
+					lastName: signupFormField_lastName,
+					email: signupFormField_email,
+				}
+			),
+		};
+		const response = await fetch('http://localhost:3003/signup', requestOptions);
+		if (response.ok) {
+			const _currentUser = await response.json();
+			setCurrentUser(prev => ({ ...prev, ..._currentUser }));
+			setSignupFormField_login('');
+			setSignupFormField_password1('');
+			setSignupFormField_password2('');
+			setSignupFormField_firstName('');
+			setSignupFormField_lastName('');
+			setSignupFormField_email('');
+		}
+	}
+
 
 	const handle_logoutForm_logoutButton = async (e) => {
 		const requestOptions = {
@@ -76,22 +139,56 @@ function App() {
 						<div><button onClick={handle_logoutForm_logoutButton}>Logout</button></div>
 					)}
 					{currentUserIsInGroup('loggedOutUsers') && (
-						<form>
-							<fieldset>
-								<legend>Login</legend>
-								<div className="row">
-									<label htmlFor="login">Login</label>
-									<input type="text" id="login" value={loginFormField_login} 	onChange={handle_loginFormField_login} />
-								</div>
-								<div className="row">
-									<label htmlFor="password">Password</label>
-									<input type="password" id="password" value={loginFormField_password} onChange={handle_loginFormField_password} />
-								</div>
-								<div className="buttonRow">
-									<button onClick={handle_loginForm_loginButton}>Submit</button>
-								</div>
-							</fieldset>
-						</form>
+						<>
+							<form>
+								<fieldset>
+									<legend>Login</legend>
+									<div className="row">
+										<label htmlFor="loginFormField_login">Login</label>
+										<input type="text" id="loginFormField_login" value={loginFormField_login} onChange={handle_loginFormField_login} />
+									</div>
+									<div className="row">
+										<label htmlFor="loginFormField_password">Password</label>
+										<input type="password" id="loginFormField_password" value={loginFormField_password} onChange={handle_loginFormField_password} />
+									</div>
+									<div className="buttonRow">
+										<button onClick={handle_loginForm_loginButton}>Submit</button>
+									</div>
+								</fieldset>
+							</form>
+							<form>
+								<fieldset>
+									<legend>Signup</legend>
+									<div className="row">
+										<label htmlFor="signupFormField_login">Login</label>
+										<input type="text" id="signupFormField_login" value={signupFormField_login} onChange={handle_signupFormField_login} />
+									</div>
+									<div className="row">
+										<label htmlFor="signupFormField_password1">Password 1</label>
+										<input type="password" id="signupFormField_password1" value={signupFormField_password1} onChange={handle_signupFormField_password1} />
+									</div>
+									<div className="row">
+										<label htmlFor="signupFormField_password2">Password 2</label>
+										<input type="password" id="signupFormField_password2" value={signupFormField_password2} onChange={handle_signupFormField_password2} />
+									</div>
+									<div className="row">
+										<label htmlFor="signupFormField_firstName">First Name</label>
+										<input type="text" id="signupFormField_firstName" value={signupFormField_firstName} onChange={handle_signupFormField_firstName} />
+									</div>
+									<div className="row">
+										<label htmlFor="signupFormField_lastName">Last Name</label>
+										<input type="text" id="signupFormField_lastName" value={signupFormField_lastName} onChange={handle_signupFormField_lastName} />
+									</div>
+									<div className="row">
+										<label htmlFor="signupFormField_email">E-Mail</label>
+										<input type="text" id="signupFormField_email" value={signupFormField_email} onChange={handle_signupFormField_email} />
+									</div>
+									<div className="buttonRow">
+										<button onClick={handle_signupForm_signupButton}>Submit</button>
+									</div>
+								</fieldset>
+							</form>
+						</>
 					)}
 
 					{currentUserIsInGroup('loggedOutUsers') && (
